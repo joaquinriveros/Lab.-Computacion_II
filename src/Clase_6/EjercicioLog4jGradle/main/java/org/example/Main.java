85f381758dc9b5cd5a7f9d66c6f8d87e5a0cad59
Joakoo2023 <joakocarp19@gmail.com>
2024-04-22 21:21:11 -0300

    Codigos

---
+++ src/Clase_6/EjercicioLog4jGradle/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class Main {
+    final Level VERBOSE=Level.forName("VERBOSE",150);
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+        Main app= new Main();
+        if(logger.isErrorEnabled())
+            logger.error("Esto es un error");
+        logger.info("Entering application");
+        logger.debug("Esto es debug Logging!");
+        logger.log(app.VERBOSE,"a verbose message");
+    }
+}
